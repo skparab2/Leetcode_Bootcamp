9a81b6e78137e0c44c38a27e0be74ed4915020aa
skparab2 <96956561+skparab2@users.noreply.github.com>
2025-03-06 22:37:57 -0500

    Add files via upload

---
+++ week 2/8. String to Integer (atoi).py
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <meta name='og:viewport' content='width=device-width,initial-scale=1'>
+    <meta name="og:description" content="Skparab1">
+    <title>Notepad</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="icon" type="image/x-icon" href="pacman-favicon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Finlandica&display=swap" rel="stylesheet"> 
+  </head>
+  <body>
+    <form>
+      <textarea class="notepad" id="notepad" autofocus>
+      </textarea>
+    </form>
+
+    <div class="fullcover" id="fullcover">Saving.</div>
+    <div class="fullcover" id="fullcover1">Loaded.</div>
+    
+    <div class="fullcover" id="fullcover2">Failed.</div>
+
+    <script src="index.js"></script>
+
+  </body>
+</html>
